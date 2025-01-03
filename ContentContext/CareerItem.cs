34a5c8877b5d50f2219c454e04ef899fefbf949f
Diogoltt <diogoleite09@gmail.com>
2025-01-02 22:57:25 -0400

    feat: Business rules

--- ContentContext/CareerItem.cs
+++ ContentContext/CareerItem.cs
@@ -1,6 +1,16 @@
 namespace ProjetoPOO.ContentContext{
     public class CareerItem
     {
+        public CareerItem(int order, string title, string description, Course course)
+        {
+            if (course == null)
+                throw new System.Exception("O curso não pode ser nulo");
+            Order = order;
+            Title = title;
+            Description = description;
+            Course = course;
+        }
+
         public int Order { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
